e4654180258f7ec8ec4ce1c0f4ddfaf474dfd278
김지호 <jiho001108@gmail.com>
2024-05-14 18:16:20 +0900

    post contents from the database by its slug : TS 만족 X

--- app/routes/posts.$slug.tsx
+++ app/routes/posts.$slug.tsx
@@ -1,20 +1,20 @@
 import type { LoaderFunctionArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
+import invariant from "tiny-invariant";
 
-export const loader = async ({
-  params,
-}: LoaderFunctionArgs) => {
-  return json({ slug: params.slug });
+import { getPost } from "~/models/post.server";
+
+export const loader = async ({ params }: LoaderFunctionArgs) => {
+  const post = await getPost(params.slug);
+	return json({ post });
 };
 
 export default function PostSlug() {
-  const { slug } = useLoaderData<typeof loader>();
-  return (
-    <main className="mx-auto max-w-4xl">
-      <h1 className="my-6 border-b-2 text-center text-3xl">
-        Some Post: {slug}
-      </h1>
-    </main>
-  );
+	const { post } = useLoaderData<typeof loader>();
+	return (
+		<main className="mx-auto max-w-4xl">
+			<h1 className="my-6 border-b-2 text-center text-3xl">{post.title}</h1>
+		</main>
+	);
 }
