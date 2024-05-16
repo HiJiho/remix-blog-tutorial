71345e65554f128f13b16fcd7b7d4885d83f8f25
김지호 <jiho001108@gmail.com>
2024-05-16 10:03:36 +0900

    Update posts.admin.new.tsx

--- app/routes/posts.admin.new.tsx
+++ app/routes/posts.admin.new.tsx
@@ -5,37 +5,38 @@ import invariant from "tiny-invariant";
 
 import { createPost } from "~/models/post.server";
 
+// 서버사이드 로직
 export const action = async ({ request }: ActionFunctionArgs) => {
-	console.log("11111111111111");
 	const formData = await request.formData();
-	console.log("222222222222222");
+
 	const title = formData.get("title");
 	const slug = formData.get("slug");
 	const markdown = formData.get("markdown");
-	console.log("333333333333333");
+
 	const errors = {
 		title: title ? null : "Title is required",
 		slug: slug ? null : "Slug is required",
 		markdown: markdown ? null : "Markdown is required",
 	};
-	console.log("4444444444444444");
+
 	const hasErrors = Object.values(errors).some((errorMessage) => errorMessage);
 	if (hasErrors) {
 		return json(errors);
 	}
-	console.log("555555555555555");
+
 	invariant(typeof title === "string", "title must be a string");
 	invariant(typeof slug === "string", "slug must be a string");
 	invariant(typeof markdown === "string", "markdown must be a string");
-	console.log("666666666666666");
+
 	await createPost({ title, slug, markdown });
-	console.log("7777777777777777");
+
 	return redirect("/posts/admin");
 };
 
 const inputClassName =
 	"w-full rounded border border-gray-500 px-2 py-1 text-lg";
 
+// 클라이언트 사이드 로직
 export default function NewPost() {
 	const errors = useActionData<typeof action>();
 
